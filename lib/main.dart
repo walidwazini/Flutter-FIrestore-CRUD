2805d0611cab472e90131eca2ebc9ffc23d4ca04
Walid Wazini <wazini25@gmail.com>
2022-01-08 12:05:54 +0800

    new start

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,140 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import './Screens/CreateUser.dart';
+void main() => runApp(StartApp());
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class StartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+            .copyWith(secondary: Colors.cyan),
       ),
-      home: HomeScreen() ,
+      home: MyApp(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? studentName, studentID, studyProgram;
+  double? studentGPA;
+
+  getStudentName(name) {
+    this.studentName = name;
+  }
+
+  getStudentID(studentId) {
+    this.studentID = studentId;
+  }
+
+  getProgramId(programName) {
+    this.studyProgram = programName;
+  }
+
+  getGpa(gpa) {
+    this.studentGPA = double.parse(gpa);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Firebase CRUD'),
+        title: Text('Flutter College'),
       ),
-      body: CreateUser(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Name',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 2.0))),
+                onChanged: (String name) {
+                  getStudentName(name);
+                },
+              ),
+            ),
+            _buildTextField('Student ID', (id) {
+              getStudentID(id);
+            }),
+            _buildTextField('Study Program', (programId) {
+              getProgramId(programId);
+            }),
+            _buildTextField('GPA', (gpa) {
+              getGpa(gpa);
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 80,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      createData();
+                    },
+                    child: Text('Create'),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                  ),
+                ),
+                _buildButton('Read', () {
+                  readData();
+                }, Colors.blue),
+                _buildButton('Update', () {
+                  updateData();
+                }, Colors.indigo),
+                _buildButton('Delete', () {
+                  deleteData();
+                }, Colors.red),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String labelText, ValueChanged onChangeHandler) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: labelText,
+            fillColor: Colors.white,
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black, width: 2.0))),
+        onChanged: onChangeHandler,
+      ),
+    );
+  }
+
+  Widget _buildButton(String title, VoidCallback onPressHandler, Color warna) {
+    return Container(
+      width: 80,
+      height: 40,
+      child: ElevatedButton(
+        onPressed: onPressHandler,
+        child: Text('Create'),
+        style: ElevatedButton.styleFrom(
+            primary: warna,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10))),
+      ),
     );
   }
 }
